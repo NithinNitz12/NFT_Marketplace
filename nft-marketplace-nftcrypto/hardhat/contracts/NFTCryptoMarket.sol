1103e71a606a0940602d0c1d29842147725eb357
Nithin <nithinraj10@hotmail.com>
2022-07-04 21:03:29 +0530

    Minted NFTs

--- nft-marketplace-nftcrypto/hardhat/contracts/NFTCryptoMarket.sol
+++ nft-marketplace-nftcrypto/hardhat/contracts/NFTCryptoMarket.sol
@@ -138,4 +138,26 @@ contract NFTCryptoMarket is ReentrancyGuard{
             }
             return items;
         }
+        
+        //Minted NFTs
+        function fetchItemsCreated() public view  returns(MarketToken[] memory){
+            uint totalItemCount = _tokenIds.current();
+            uint itemCount = 0;
+            uint currentIndex = 0;
+
+            for(uint i=0;i<totalItemCount;i++){
+                if(idToMarketToken[i+1].seller == msg.sender){
+                    itemCount += 1;
+                }
+            }
+            MarketToken[] memory items = new MarketToken[](itemCount);
+            for(uint i=0;i<totalItemCount;i++){
+                if(idToMarketToken[i+1].seller == msg.sender){
+                    uint currentId = idToMarketToken[i+1].itemId;
+                    MarketToken storage currentItem = idToMarketToken[currentId];
+                    items[currentIndex] = currentItem;currentIndex += 1;
+                }
+            }
+            return items;
+        }
     }
