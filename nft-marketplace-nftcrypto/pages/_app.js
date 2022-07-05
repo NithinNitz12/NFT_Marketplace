0ade4b0ce0e8f2417278cc69a85b4b0eeabaf47b
Nithin <nithinraj10@hotmail.com>
2022-07-05 19:13:16 +0530

    Frontend creation

--- nft-marketplace-nftcrypto/pages/_app.js
+++ nft-marketplace-nftcrypto/pages/_app.js
@@ -1,7 +1,37 @@
 import '../styles/globals.css'
+import Link from 'next/link'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function NFTCryptoMarketplace({Component, pageProps}){
+  return(
+    <div>
+      <nav className='border-b p-6' style={{backgroundColor:'purple'}}>
+        <p className='text-4x1 font-bold text-white'>NFTCrypto Marketplace</p>
+        <div className='flex mt-4 justify-center'>
+          <Link href='/'>
+            <a className='mr-4'>
+              Main Marketplace
+            </a>
+          </Link>
+          <Link href='/mint-item'>
+            <a className='mr-6'>
+              Mint Tokens
+            </a>
+          </Link>
+          <Link href='/my-nfts'>
+            <a className='mr-6'>
+              My NFTs
+            </a>
+          </Link>
+          <Link href='/account-dashboard'>
+            <a className='mr-6'>
+              Account Dashboard
+            </a>
+          </Link>
+        </div>
+      </nav>
+      <Component {...pageProps}/>
+    </div>
+  )
 }
 
-export default MyApp
+export default NFTCryptoMarketplace
