fa5accddb005bfbd12ee653f9f06ef1ad293c594
Nithin <nithinraj10@hotmail.com>
2022-07-05 15:49:36 +0530

    sample-test.js completed

--- nft-marketplace-nftcrypto/test/sample-test.js
+++ nft-marketplace-nftcrypto/test/sample-test.js
@@ -37,7 +37,21 @@ describe("NFTCryptoMarket", function () {
       value: auctionPrice
     })
 
-    const items = await market.fetchMarketTokens()
+    let items = await market.fetchMarketTokens()
+
+    items = await Promise.all(items.map(async i => {
+      //get the uri of the value
+
+      const tokenUri = await nft.tokenURI(i.tokenId)
+      let item = {
+        price: i.price.toString(),
+        tokenId: i.tokenId.toString(),
+        seller: i.seller,
+        owner: i.owner,
+        tokenUri
+      }
+      return item;
+    }))
 
     //test out all the items
     console.log('items', items)
