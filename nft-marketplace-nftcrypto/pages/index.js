7ed7e987b82f7342f46b1ff1f159e3246d733ef4
Nithin <nithinraj10@hotmail.com>
2022-07-06 21:55:33 +0530

    Errors not resolved!!!

--- nft-marketplace-nftcrypto/pages/index.js
+++ nft-marketplace-nftcrypto/pages/index.js
@@ -9,8 +9,8 @@ import NFT from '../artifacts/contracts/NFT.sol/NFT.json'
 import NFTCryptoMarket from '../artifacts/contracts/NFTCryptoMarket.sol/NFTCryptoMarket.json'
 
 export default function Home() {
-  const [nfts, setNfts] = useState([])
-  const [loadingstate, setLoadingState] = useState('not-loaded')
+  const [nfts, setNFts] = useState([])
+  const [loadingState, setLoadingState] = useState('not-loaded')
 
   useEffect(()=> {
     loadNFTs()
@@ -23,7 +23,7 @@ export default function Home() {
     const provider = new ethers.providers.JsonRpcProvider()
     const tokenContract = new ethers.Contract(nftaddress, NFT.abi, provider)
     const marketContract = new ethers.Contract(nftmarketaddress, NFTCryptoMarket.abi, provider)
-    const data = await marketContract.fetchMarketItems()
+    const data = await marketContract.fetchMarketTokens()
 
     const items = await Promise.all(data.map(async i => {
       const tokenUri = await tokenContract.tokenURI(i.tokenId)
@@ -42,7 +42,7 @@ export default function Home() {
       return item
     }))
 
-    setNfts(items)
+    setNFts(items)
     setLoadingState('loaded')
   }
 
@@ -52,11 +52,11 @@ export default function Home() {
     const web3Modal = new Web3Modal()
     const connection = await web3Modal.connect()
     const provider = new ethers.providers.Web3Provider(connection)
+    // const provider = new ethers.providers.AlchemyProvider("rinkeby")//100
     const signer = provider.getSigner()
     const contract = new ethers.Contract(nftmarketaddress, NFTCryptoMarket.abi, signer)
 
     const price = ethers.utils.parseUnits(nft.price.toString(), 'ether')
-
     const transaction = await contract.createMarketSale(nftaddress, nft.tokenId, {
       value: price
     })
@@ -64,10 +64,35 @@ export default function Home() {
     await transaction.wait()
     loadNFTs()
   }
-
-  return (
-    <div>
-      {/* NFTCrypto MARKETPLACE */}
+  if(loadingState === 'loaded' && !nfts.length) return (<h1 
+    className='px-20 py-7 text-4x1'>No NFTs in marketplace</h1>)
+  
+    return (
+    <div className='flex justify-center'>
+      <div className='px-4' style={{maxWidth: '1600px' }}>
+        <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 pt-4'>
+          {
+            nfts.map((nft, i)=>(
+              <div key={i} className='border shadow rounded-x1 overflow-hidden'>
+                <img src={nft.image} />
+                <div className='p-4'>
+                  <p style={{height:'64px'}} className='text-3x1 font-semibold'>
+                    {nft.name}</p>
+                    <div style={{height:'72px', overflow:'hidden'}}>
+                      <p className='text-gray-400'>{nft.description}</p>
+                    </div>
+                </div>
+              <div className='p-4 bg-black'>
+                <p className='text-3x-1 mb-4 font-bold text-white'>{nft.price} ETH</p>
+                <button className='w-full bg-purple-500 text-white font-bold py-3 px-12 rounded'
+                onClick={()=> buyNFT(nft)}>Buy
+                </button>
+              </div>
+              </div>
+            ))
+          }
+        </div>
+      </div>
     </div>
   )
 }
